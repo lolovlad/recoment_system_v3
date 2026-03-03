9ce3f14d1e8607267a280d0589a1553864784765
lolovlad <lolovlad33@gmail.com>
2026-03-03 21:15:22 +0400

    laba 1

--- src/recommender_system/domain/interfaces.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from .entities import UserHistory, Recommendation
-
-
-class Recommender(ABC):
-
-    @abstractmethod
-    def get_recommendations(self, history: UserHistory) -> Recommendation:
-        pass
